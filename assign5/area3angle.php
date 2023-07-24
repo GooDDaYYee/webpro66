ddf61cd48153d03fe45bef360a1efa3eaa147874
GooDDaYYee <gooddayyee@gmail.com>
2023-07-24 14:00:02 +0700

    up

---
+++ assign5/area3angle.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>คำนวณหาพื้นที่สามเหลียม</title>
+<style type="text/css">
+	div{
+		margin: 20px;
+	}
+</style>
+<meta charset="utf-8">
+</head>
+<body>
+ <?php
+    require("function.php");
+    echo "พื้นที่สามเหลี่ยม =".area3angle(10,15),"ตารางหน่วย";
+ ?>
+</body>
+</html>
---
+++ assign5/function.php
@@ -0,0 +1,9 @@
+<?php
+    //ฟังก์ชั่นเพื่อคำนวณหาพื้นที่ สามเหลียม
+    // สูตร 0.5*ฐาน*สูง
+
+    function area3angle($base=1,$hieght=2){
+        $area3=0.5*$base*$hieght;
+        return $area3;
+    }
+?>
